fe3898456ca6d1b6a0bf909fc4bd8c8b2062a350
John Compitello <j301600@gmail.com>
2021-06-01 08:26:12 -0400

    Added intellij

--- mac-init.sh
+++ mac-init.sh
@@ -1,3 +1,3 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install --cask firefox visual-studio-code spotify alfred iterm2 zulip obsidian
+brew install --cask firefox visual-studio-code spotify alfred iterm2 zulip obsidian intellij-idea-ce
 brew install git gh
