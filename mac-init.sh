d03c30374439b3b60659d8517c0cd1351bfcce95
John Compitello <j301600@gmail.com>
2021-06-01 16:27:02 -0400

    include miniconda

--- mac-init.sh
+++ mac-init.sh
@@ -1,3 +1,3 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install --cask firefox visual-studio-code spotify alfred iterm2 zulip obsidian intellij-idea-ce rectangle
+brew install --cask firefox visual-studio-code spotify alfred iterm2 zulip obsidian intellij-idea-ce rectangle miniconda
 brew install git gh
