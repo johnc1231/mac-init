445461b1432d4aee7efc8243055400f7edaa7385
John Compitello <j301600@gmail.com>
2021-05-31 20:06:06 -0400

    Initial setup file

---
+++ mac-init.sh
@@ -0,0 +1,3 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask firefox visual-studio-code spotify alfred iterm2 zulip obsidian
+brew install git
